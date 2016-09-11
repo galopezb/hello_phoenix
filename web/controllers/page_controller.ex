d146d6a37adae12efd4521a2da8e64d75cfb11ff
gualopezb <gustavo@kommit.co>
2016-09-11 12:26:03 -0500

    Add some views

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,15 @@
 defmodule HelloPhoenix.PageController do
   use HelloPhoenix.Web, :controller
 
+  def show(conn, _params) do
+    page = %{title: "foo"}
+
+    render conn, "show.json", page: page
+  end
+
   def index(conn, _params) do
-    render conn, "index.html"
+    pages = [%{title: "foo"}, %{title: "bar"}]
+
+    render conn, "index.json", pages: pages
   end
 end
