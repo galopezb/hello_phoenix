4a8e7ebab226fc2ce5f9593d53166e4281c24fad
gualopezb <gustavo@kommit.co>
2016-09-11 12:30:18 -0500

    Remove JSON responses from page controller

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,15 +1,7 @@
 defmodule HelloPhoenix.PageController do
   use HelloPhoenix.Web, :controller
 
-  def show(conn, _params) do
-    page = %{title: "foo"}
-
-    render conn, "show.json", page: page
-  end
-
   def index(conn, _params) do
-    pages = [%{title: "foo"}, %{title: "bar"}]
-
-    render conn, "index.json", pages: pages
+    render conn, "index.html"
   end
 end
